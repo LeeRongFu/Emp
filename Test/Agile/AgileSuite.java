94d0fbd1152870f95bcaf8a043fffb12ccc68505
TARUC <TARUC@PC-98>
2016-12-07 15:36:02 +0800

    new commit

---
+++ Test/Agile/AgileSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Agile;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author TARUC
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Agile.EmployeeTest.class})
+public class AgileSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
